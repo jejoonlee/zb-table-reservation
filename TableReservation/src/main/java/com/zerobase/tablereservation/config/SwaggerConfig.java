37b71df57c8fdba5bd7c210dc91d587fcc25b7a6
jejoonlee <jejoonlee1996@gmail.com>
2023-09-22 13:32:19 +0900

    SwaFEAT: swagger 미리 구현해 놓기

---
+++ TableReservation/src/main/java/com/zerobase/tablereservation/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.zerobase.tablereservation.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    private String TITLE = "테이블 예약하기 어플리케이션";
+    private String DESCRIPTION = "테이블을 원격으로 예약을 하고, 10분 전까지 매장에 도착면 매장을 이용할 수 있는 서비스";
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(TITLE)
+                .description(DESCRIPTION)
+                .version("1.0")
+                .build();
+    }
+}
